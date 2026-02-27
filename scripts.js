0c340e32a8cc297fa21d20c70fd87f6ffd2b5521
cxtgirl <eddievshen@gmail.com>
2026-02-27 18:18:20 -0500

    nya

--- scripts.js
+++ scripts.js
@@ -12,13 +12,34 @@ function time() {
     document.getElementById("time").innerHTML = `
 uhhhhhhh hi! basically I'm catgirl mrrow mrrp :3333
 <br>here are <a href="https://en.pronouns.page/@cxtgirl" target="_blank" title="pronounce">my pronouns</a> (woke)
-<br><a href="https://www.bu.edu/" target="_blank" title="waow what could this be">BU</a> class of 2029, majoring in astronomy & physics (waow space)
+<br><a href="https://www.bu.edu/" target="_blank" title="waow what could this be" >BU</a> class of 2029, majoring in astronomy & physics (waow space)
 <br>my current time is ${mytime}!
 <br>in my free time, I like to eat, sleep, and torture myself in various games.
 <br>burger enjoyer... they're so tasty!!! also I'm extremely fond of salmon sushi.
 <br>currently, I'm learning how to create this website! you can find more information in the <a href="siteinfo.html" title="site info!">site info</a> section.`
 
 }
+
+const coin2 = document.getElementById("coin");
+let headcount = 0;
+let tailcount = 0;
+function gamble() {
+    const result = Math.floor(Math.random()*2);
+    let textresult;
+        if (result === 1) {
+            textresult = "heads";
+            headcount++;
+        } else {
+            textresult = "tails";
+            tailcount++;
+        }
+    document.getElementById("coinflip").innerHTML = `result: ${textresult}
+    <br>heads: ${headcount}
+    <br>tails: ${tailcount}`
+    
+}
+
+coin2.addEventListener("click", gamble);
 
 time();
 setInterval(time, 1000);
